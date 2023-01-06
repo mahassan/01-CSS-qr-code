bcaae2c4cf9adf358a246d62d5669735dd765148
Ammar Hassan <ammar@ammarhassan.co.uk>
2023-01-06 10:24:49 +0000

    changes to bg colour and misc

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'regal-blue': '#D7E0EE'
+      }
+    },
   },
   plugins: [],
 }
